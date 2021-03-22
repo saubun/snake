807f5221d8a06c3937d92acc6b9769aac2a916e3
saubun <dameon0430@gmail.com>
2021-03-22 09:10:01 -0400

    Finished :D

--- dist/render.js
+++ dist/render.js
@@ -2,8 +2,12 @@ const board_border = 'black';
 const board_background = 'white';
 const snake_color = 'lightblue';
 const snake_border = 'darkblue';
+const food_color = 'orange';
+const food_border = 'darkorange';
 let dx = 10;
 let dy = 0;
+let foodcount = 0;
+let score = 0;
 let alive = true;
 let snake = [
     { x: 200, y: 200 },
@@ -15,23 +19,30 @@ let snake = [
     { x: 140, y: 200 },
     { x: 130, y: 200 },
 ];
+let foodPos = {
+    x: 100,
+    y: 100,
+};
 const snakeboard = document.getElementById('snakeboard');
 const ctx = snakeboard.getContext('2d');
-const lostMessage = document.getElementById('lost-message');
 document.addEventListener('keydown', (e) => {
-    if (e.code === 'KeyW' || e.code === 'ArrowUp') {
+    const goingUp = dy === -10 && dx === 0;
+    const goingDown = dy === 10 && dx === 0;
+    const goingRight = dy === 0 && dx === 10;
+    const goingLeft = dy === 0 && dx === -10;
+    if ((e.code === 'KeyW' || e.code === 'ArrowUp') && !goingDown) {
         dy = -10;
         dx = 0;
     }
-    else if (e.code === 'KeyS' || e.code === 'ArrowDown') {
+    else if ((e.code === 'KeyS' || e.code === 'ArrowDown') && !goingUp) {
         dy = 10;
         dx = 0;
     }
-    else if (e.code === 'KeyA' || e.code === 'ArrowLeft') {
+    else if ((e.code === 'KeyA' || e.code === 'ArrowLeft') && !goingRight) {
         dy = 0;
         dx = -10;
     }
-    else if (e.code === 'KeyD' || e.code === 'ArrowRight') {
+    else if ((e.code === 'KeyD' || e.code === 'ArrowRight') && !goingLeft) {
         dy = 0;
         dx = 10;
     }
@@ -44,6 +55,8 @@ function main() {
             borderCheck();
             moveSnake();
             drawSnake();
+            drawFood();
+            foodCollisionCheck();
         }
         main();
     }, 100);
@@ -73,10 +86,7 @@ function moveSnake() {
 }
 function borderCheck() {
     const head = snake[0];
-    if (head.x < 20 ||
-        head.y < 20 ||
-        head.x > snakeboard.width - 30 ||
-        head.y > snakeboard.height - 30) {
+    if (head.x < 0 || head.y < 0 || head.x > snakeboard.width || head.y > snakeboard.height) {
         alive = false;
     }
 }
@@ -87,6 +97,35 @@ function selfCollisionCheck() {
             alive = false;
         }
     });
+}
+function foodCollisionCheck() {
+    const head = snake[0];
+    if (head.x < foodPos.x + 10 &&
+        head.x + 10 > foodPos.x &&
+        head.y < foodPos.y + 10 &&
+        head.y + 10 > foodPos.y) {
+        updateScore();
+        spawnFood();
+        increaseLength();
+    }
+}
+function spawnFood() {
+    let posX = Math.round(((Math.random() * 400) / 10) * 10);
+    let posY = Math.round(((Math.random() * 400) / 10) * 10);
+    foodPos = { x: posX, y: posY };
+}
+function drawFood() {
+    ctx.fillStyle = food_color;
+    ctx.strokeStyle = food_border;
+    ctx.fillRect(foodPos.x, foodPos.y, 10, 10);
+    ctx.strokeRect(foodPos.x, foodPos.y, 10, 10);
+}
+function updateScore() {
+    score += 1;
+    document.getElementById('score').innerHTML = score.toString();
+}
+function increaseLength() {
+    snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
 }
 window.onload = main;
 //# sourceMappingURL=render.js.map
--- src/render.ts
+++ src/render.ts
@@ -2,10 +2,14 @@ const board_border = 'black';
 const board_background = 'white';
 const snake_color = 'lightblue';
 const snake_border = 'darkblue';
+const food_color = 'orange';
+const food_border = 'darkorange';
+
 let dx: number = 10;
 let dy: number = 0;
-
-let alive = true;
+let foodcount: number = 0;
+let score: number = 0;
+let alive: boolean = true;
 
 interface Vector2 {
 	x: number;
@@ -24,23 +28,34 @@ let snake: Vector2[] = [
 	{ x: 130, y: 200 },
 ];
 
+// Position of current piece of food
+let foodPos: Vector2 = {
+	x: 100,
+	y: 100,
+};
+
 // Get context
 const snakeboard = document.getElementById('snakeboard')! as HTMLCanvasElement;
 const ctx = snakeboard.getContext('2d')! as CanvasRenderingContext2D;
-const lostMessage = document.getElementById('lost-message') as HTMLDivElement;
 
 // Input
-document.addEventListener('keydown', (e) => {
-	if (e.code === 'KeyW' || e.code === 'ArrowUp') {
+document.addEventListener('keydown', (e): void => {
+	// Don't let player just run into self by pressing opposite direction
+	const goingUp = dy === -10 && dx === 0;
+	const goingDown = dy === 10 && dx === 0;
+	const goingRight = dy === 0 && dx === 10;
+	const goingLeft = dy === 0 && dx === -10;
+
+	if ((e.code === 'KeyW' || e.code === 'ArrowUp') && !goingDown) {
 		dy = -10;
 		dx = 0;
-	} else if (e.code === 'KeyS' || e.code === 'ArrowDown') {
+	} else if ((e.code === 'KeyS' || e.code === 'ArrowDown') && !goingUp) {
 		dy = 10;
 		dx = 0;
-	} else if (e.code === 'KeyA' || e.code === 'ArrowLeft') {
+	} else if ((e.code === 'KeyA' || e.code === 'ArrowLeft') && !goingRight) {
 		dy = 0;
 		dx = -10;
-	} else if (e.code === 'KeyD' || e.code === 'ArrowRight') {
+	} else if ((e.code === 'KeyD' || e.code === 'ArrowRight') && !goingLeft) {
 		dy = 0;
 		dx = 10;
 	}
@@ -55,6 +70,8 @@ function main(): void {
 			borderCheck();
 			moveSnake();
 			drawSnake();
+			drawFood();
+			foodCollisionCheck();
 		}
 		main();
 	}, 100);
@@ -95,13 +112,8 @@ function moveSnake(): void {
 function borderCheck(): void {
 	const head = snake[0];
 
-	// Numbers are weird because edges aren't the same as the size ???
-	if (
-		head.x < 20 ||
-		head.y < 20 ||
-		head.x > snakeboard.width - 30 ||
-		head.y > snakeboard.height - 30
-	) {
+	// Borders are weird :/
+	if (head.x < 0 || head.y < 0 || head.x > snakeboard.width || head.y > snakeboard.height) {
 		alive = false;
 	}
 }
@@ -110,11 +122,58 @@ function borderCheck(): void {
 function selfCollisionCheck(): void {
 	const head = snake[0];
 
+	// Slice so head isn't included
 	snake.slice(1).forEach((part) => {
 		if (head.x === part.x && head.y === part.y) {
 			alive = false;
 		}
 	});
+}
+
+// Checks for collision with food <3
+function foodCollisionCheck() {
+	const head = snake[0];
+
+	// Collision detection logic from
+	// https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+	if (
+		head.x < foodPos.x + 10 &&
+		head.x + 10 > foodPos.x &&
+		head.y < foodPos.y + 10 &&
+		head.y + 10 > foodPos.y
+	) {
+		updateScore();
+		spawnFood();
+		increaseLength();
+	}
+}
+
+// Spawn a piece of food when one is eaten
+function spawnFood() {
+	// Rounding to tens still isn't working :/
+	let posX = Math.round(((Math.random() * 400) / 10) * 10);
+	let posY = Math.round(((Math.random() * 400) / 10) * 10);
+
+	foodPos = { x: posX, y: posY };
+}
+
+// Draw the food
+function drawFood() {
+	ctx.fillStyle = food_color;
+	ctx.strokeStyle = food_border;
+	ctx.fillRect(foodPos.x, foodPos.y, 10, 10);
+	ctx.strokeRect(foodPos.x, foodPos.y, 10, 10);
+}
+
+// Increase score by one
+function updateScore() {
+	score += 1;
+	document.getElementById('score')!.innerHTML = score.toString();
+}
+
+// Increase the length of the snake
+function increaseLength() {
+	snake.push({ x: snake[snake.length - 1].x, y: snake[snake.length - 1].y });
 }
 
 // Start the game
