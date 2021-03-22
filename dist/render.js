d6c1709e23b23a4a33ad09fbfc4cf3274b1b58d4
saubun <dameon0430@gmail.com>
2021-03-21 20:47:12 -0400

    progress

--- dist/render.js
+++ dist/render.js
@@ -2,6 +2,9 @@ const board_border = 'black';
 const board_background = 'white';
 const snake_color = 'lightblue';
 const snake_border = 'darkblue';
+let dx = 10;
+let dy = 0;
+let alive = true;
 let snake = [
     { x: 200, y: 200 },
     { x: 190, y: 200 },
@@ -11,10 +14,36 @@ let snake = [
 ];
 const snakeboard = document.getElementById('snakeboard');
 const ctx = snakeboard.getContext('2d');
-window.onload = () => {
-    clearCanvas();
-    drawSnake();
-};
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'KeyW' || e.code === 'ArrowUp') {
+        dy = -10;
+        dx = 0;
+    }
+    else if (e.code === 'KeyS' || e.code === 'ArrowDown') {
+        dy = 10;
+        dx = 0;
+    }
+    else if (e.code === 'KeyA' || e.code === 'ArrowLeft') {
+        dy = 0;
+        dx = -10;
+    }
+    else if (e.code === 'KeyD' || e.code === 'ArrowRight') {
+        dy = 0;
+        dx = 10;
+    }
+});
+function main() {
+    setTimeout(() => {
+        if (alive) {
+            clearCanvas();
+            moveSnake();
+            borderCheck();
+            selfCollisionCheck();
+            drawSnake();
+        }
+        main();
+    }, 100);
+}
 function clearCanvas() {
     ctx.fillStyle = board_background;
     ctx.strokeStyle = board_border;
@@ -30,4 +59,25 @@ function drawSnakePart(snakePart) {
     ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
     ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
+function moveSnake() {
+    const head = {
+        x: snake[0].x + dx,
+        y: snake[0].y + dy,
+    };
+    snake.unshift(head);
+    snake.pop();
+}
+function borderCheck() {
+    const head = {
+        x: snake[0].x,
+        y: snake[0].y,
+    };
+}
+function selfCollisionCheck() {
+    const head = {
+        x: snake[0].x,
+        y: snake[0].y,
+    };
+}
+window.onload = main;
 //# sourceMappingURL=render.js.map
--- src/render.ts
+++ src/render.ts
@@ -2,12 +2,17 @@ const board_border = 'black';
 const board_background = 'white';
 const snake_color = 'lightblue';
 const snake_border = 'darkblue';
+let dx: number = 10;
+let dy: number = 0;
+
+let alive = true;
 
 interface Vector2 {
 	x: number;
 	y: number;
 }
 
+// Snake body
 let snake: Vector2[] = [
 	{ x: 200, y: 200 },
 	{ x: 190, y: 200 },
@@ -16,14 +21,42 @@ let snake: Vector2[] = [
 	{ x: 160, y: 200 },
 ];
 
-const snakeboard = document.getElementById('snakeboard') as HTMLCanvasElement;
-const ctx = snakeboard.getContext('2d');
+// Get context
+const snakeboard = document.getElementById('snakeboard')! as HTMLCanvasElement;
+const ctx = snakeboard.getContext('2d')! as CanvasRenderingContext2D;
 
-window.onload = (): void => {
-	clearCanvas();
-	drawSnake();
-};
+// Input
+document.addEventListener('keydown', (e) => {
+	if (e.code === 'KeyW' || e.code === 'ArrowUp') {
+		dy = -10;
+		dx = 0;
+	} else if (e.code === 'KeyS' || e.code === 'ArrowDown') {
+		dy = 10;
+		dx = 0;
+	} else if (e.code === 'KeyA' || e.code === 'ArrowLeft') {
+		dy = 0;
+		dx = -10;
+	} else if (e.code === 'KeyD' || e.code === 'ArrowRight') {
+		dy = 0;
+		dx = 10;
+	}
+});
 
+// Main function
+function main(): void {
+	setTimeout(() => {
+		if (alive) {
+			clearCanvas();
+			moveSnake();
+			borderCheck();
+			selfCollisionCheck();
+			drawSnake();
+		}
+		main();
+	}, 100);
+}
+
+// Clears the canvas with a white rectangle
 function clearCanvas(): void {
 	ctx.fillStyle = board_background;
 	ctx.strokeStyle = board_border;
@@ -32,15 +65,43 @@ function clearCanvas(): void {
 }
 
 // Draw the snake on the canvas
-function drawSnake() {
-	// Draw each part
+function drawSnake(): void {
 	snake.forEach(drawSnakePart);
 }
 
 // Draw one snake part
-function drawSnakePart(snakePart: Vector2) {
+function drawSnakePart(snakePart: Vector2): void {
 	ctx.fillStyle = snake_color;
 	ctx.strokeStyle = snake_border;
 	ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
 	ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 }
+
+// Moves snake position based on dx and dy
+function moveSnake(): void {
+	const head: Vector2 = {
+		x: snake[0].x + dx,
+		y: snake[0].y + dy,
+	};
+	snake.unshift(head);
+	snake.pop();
+}
+
+// Checks if the snake touches the border
+function borderCheck(): void {
+	const head: Vector2 = {
+		x: snake[0].x,
+		y: snake[0].y,
+	};
+}
+
+// Checks if the snake touches itself
+function selfCollisionCheck(): void {
+	const head: Vector2 = {
+		x: snake[0].x,
+		y: snake[0].y,
+	};
+}
+
+// Start the game
+window.onload = main;
